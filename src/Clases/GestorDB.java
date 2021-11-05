13d9b8ad831d17b5f11f848f4a4fe9a39d88f7f5
Julen Alboni <julen.albonigamenor@opendeusto.es>
2021-11-05 15:30:56 +0100

    JavaDoc gestorDB

--- src/Clases/GestorDB.java
+++ src/Clases/GestorDB.java
@@ -5,11 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Clase estática para gestionar la conexión con la base de datos
+ * @author Pablo Alberdi
+ * @author Julen Alboniga-Menor
+ * @author Leire Gurrutxaga
+ *
+ */
 public class GestorDB {
 	private static String db = "Cadena.db";
 	private static String url = "jdbc:sqlite:" + db;
 	private static Connection con;
 	
+	/**
+	 * Establece la conexión con la base de datos
+	 */
 	public static void conectar() {
 		try {
 			con = DriverManager.getConnection(url);
@@ -21,6 +31,9 @@ public class GestorDB {
 		}
 	}
 	
+	/**
+	 * Genera las tablas necesarias en caso de que éstas no hayan sido creadas
+	 */
 	private static void crearTablas() {
 		try {
 			Statement stmt = con.createStatement();
