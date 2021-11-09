e0e430ecf194f86efbcd01bd605e73f218f80103
Pablo Alberdi <palberdi2000@opendeusto.es>
2021-11-09 13:05:58 +0100

    Ventanas

--- src/ventanas/PantallaFinal.java
+++ src/ventanas/PantallaFinal.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class PantallaFinal extends JFrame {
 
@@ -35,8 +37,28 @@ public class PantallaFinal extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Su pedido se ha realizado correctamente.");
+		lblNewLabel.setBounds(71, 73, 263, 29);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Finalizar");
+		btnNewButton.setBounds(123, 184, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("En breves momentos se realizará el envio.");
+		lblNewLabel_1.setBounds(71, 101, 277, 24);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Muchas gracias");
+		lblNewLabel_2.setBounds(137, 137, 125, 16);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Confirmación de pedido");
+		lblNewLabel_3.setBounds(114, 31, 190, 16);
+		contentPane.add(lblNewLabel_3);
 	}
 
 }
--- src/ventanas/PantallaPedido.java
+++ src/ventanas/PantallaPedido.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class PantallaPedido extends JFrame {
 
@@ -35,8 +37,27 @@ public class PantallaPedido extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Pedido Total:");
+		lblNewLabel.setBounds(48, 53, 115, 25);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Dirección de entrega:");
+		lblNewLabel_1.setBounds(48, 108, 169, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Fecha pedido:");
+		lblNewLabel_2.setBounds(48, 160, 156, 16);
+		contentPane.add(lblNewLabel_2);
+		
+		JButton btnNewButton = new JButton("Realizar Pedido");
+		btnNewButton.setBounds(206, 204, 156, 29);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_3 = new JLabel("DATOS DEL PEDIDO");
+		lblNewLabel_3.setBounds(169, 6, 176, 26);
+		contentPane.add(lblNewLabel_3);
 	}
-
 }
